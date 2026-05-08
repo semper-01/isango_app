09099a12e46da8f805540a1588b14246f3abfe39
semper-01 <mleonce99@gmail.com>
2026-05-08 21:50:06 +0200

    added new routes to  login and signup screens

--- lib/core/constants/app_routes.dart
+++ lib/core/constants/app_routes.dart
@@ -2,12 +2,14 @@ class AppRoutes {
   static const login = '/login';
   static const signUp = '/signup';
   static const verifyEmail = '/verify-email';
+  static const forgotPassword = '/forgot-password';
+  static const terms = '/terms';
+  static const privacy = '/privacy';
   static const home = '/';
   static const eventDetail = '/event-detail';
   static const submitEvent = '/submit-event';
   static const saved = '/saved';
   static const profile = '/profile';
   static const settings = '/settings';
-
 
 }
